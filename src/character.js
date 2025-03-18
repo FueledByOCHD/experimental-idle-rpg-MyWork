941e0ecfec2e5efd0bc7bba9a496583e4458addf
GauTech <gregoryedwardsgau@gmail.com>
2025-03-18 20:27:14 +0000

    Update character.js

--- src/character.js
+++ src/character.js
@@ -81,9 +81,9 @@ character.stats.multiplier = {
 
 character.xp_bonuses = {};
 character.xp_bonuses.total_multiplier = {
-        hero: 100,
-        all: 100,
-        all_skill: 100,
+        hero: 10000,
+        all: 10000,
+        all_skill: 10000,
 };
 
 character.xp_bonuses.multiplier = {
